c5353843ef6623a0e5c3492856789b2712fd5861
davidakoontz <david@koontz.name>
2022-04-13 22:03:38 -0500

    Initial Commit

---
+++ MyStocks/MyStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStocksApp.swift
+//  MyStocks
+//
+//  Created by David on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MyStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
